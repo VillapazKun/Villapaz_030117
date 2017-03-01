60bacded19d438363ca4db52ffb38cf6c3f15897
Villapaz24 <villapaz@yahoo.com>
2017-03-01 20:52:43 +0800

    dsd

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.get('/about', function(req, res){
 app.get('*', function(req, res){
   res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
 });
+app.get('Calculator', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/calculator.html'));
+});
 //express server listen
 var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port ',app.get('port'));
